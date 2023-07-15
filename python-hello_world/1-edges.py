4368babd0d8b574a08baa9c6a70e518fcc68bbe3
mohmedEldeeb <eldeebmahammed@gmail.com>
2023-07-16 02:20:47 +0300

    dd

---
+++ python-hello_world/1-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_3 = word[:3]
+word_2 = word[-2:]
+middle_word = word[1:-1]
+print("First 3 letters: {}".format(word_3))
+print("Last 2 letters: {}".format(word_2))
+print("Middle word: {}".format(middle_word))
