a480a1e62c6dabcd9cbf7a25959067035b1c2f09
mohmedEldeeb <eldeebmahammed@gmail.com>
2023-07-28 06:32:13 -0700

    ddd

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -2,14 +2,15 @@
 import sys
 
 def printArges():
-    count_args = len(argv)
     argv = sys.argv[1:]
+    count_args = len(argv)
+   
     if count_args == 0:
         print(count_args, "arguments.",end="\n")
     elif count_args == 1:
         print(count_args, "argument:",end="\n")
     else:
-        print(num_argue, "arguments:",end="\n")
+        print(count_args, "arguments:",end="\n")
 
         for i, arg in enumerate(argv, 1):
             print("{}: {}".format(i, arg))
