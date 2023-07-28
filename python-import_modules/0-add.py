24eb9637b4c4bfcf3bce840d93365ca778431474
mohmedEldeeb <eldeebmahammed@gmail.com>
2023-07-28 15:33:33 +0300

    python_import

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from 0-add import add
-a = 1
-b = 2
-sum = adds.add(a, b)
-print("{} + {} = {}".format(a, b, sum))
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    sum = adds.add(a, b)
+    print("{} + {} = {}".format(a, b, sum))
