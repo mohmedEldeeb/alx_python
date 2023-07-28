c6b0eaac97d0074b86e5e4fdc28d93b2cf40f48c
mohmedEldeeb <eldeebmahammed@gmail.com>
2023-07-28 15:31:07 +0300

    python_import

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+from 0-add import add
+a = 1
+b = 2
+sum = adds.add(a, b)
+print("{} + {} = {}".format(a, b, sum))
---
+++ python-import_modules/README.md
@@ -0,0 +1,3 @@
+# alx_python
+alx_python
+python-import_modules
