1cf88991b7b28dd87f87081c1c4aff890df7d270
mohmedEldeeb <eldeebmahammed@gmail.com>
2023-07-29 10:24:13 -0700

    data_structer_1

---
+++ python-data_structures/1-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+  for line in matrix:
+    for col in line:
+      if col != line[-1]:
+        print("{:d}".format(col),end=" ")
+      else:
+        print("{:d}".format(col),end="")
+    print()
+
