4537ca16f176112cb73c089cdb658922e846a43b
mohmedEldeeb <eldeebmahammed@gmail.com>
2023-07-30 00:22:48 -0700

    more

---
+++ python-more_data_structures/3-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+
+    hight_score = max(list(a_dictionary.values()))
+    for key in a_dictionary:
+        if a_dictionary.get(key) == hight_score:
+            return (key)
+
