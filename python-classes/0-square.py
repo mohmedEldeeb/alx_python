6ee2141356a9549fbf2bce0b6858bda72deaa91f
mohmedEldeeb <eldeebmahammed@gmail.com>
2023-08-01 08:37:58 -0700

    class

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+"""
+this is semple module
+contain class
+"""
+
 class Square:
+    """"
+    class named square 
+    """
+    
     def __init__(self, size):
+        """ initlaize intanse orm class"""
         self.__size = size
 
