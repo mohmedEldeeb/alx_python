f4750ec3eac95dbdb90f3688c5f97ab1594ee650
mohmedEldeeb <eldeebmahammed@gmail.com>
2023-08-05 02:29:20 +0300

    ddd

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -5,4 +5,9 @@ class BaseGeometry:
     """
     this is a semple class to represent a empty class
     """
-    pass
+    def __dir__(cls):
+        """
+        this is a semple class to represent a empty class
+        """
+        attributes = super().__dir__()
+        return [attr for attr in attributes if attr != '__init_subclass__']
