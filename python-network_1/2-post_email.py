35d10840f2841a8bd42fce174f6c4e794e3fcd2d
mohmedEldeeb <eldeebmahammed@gmail.com>
2023-08-12 20:13:29 +0300

    data

---
+++ python-network_1/2-post_email.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+    Write a Python script that takes in a URL and an email address,
+    sends a POST request to the passed URL with the email as a parameter,
+"""
+import requests
+import sys
+
+
+def main():
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    data = {'email': email}
+    res = requests.post(url, data)
+
+    print(res.text)
+
+
+if __name__ == "__main__":
+    main()
