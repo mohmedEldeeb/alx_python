cdfce10646ceb0e47288e3b8f66bd0503b102a14
mohmedEldeeb <eldeebmahammed@gmail.com>
2023-10-13 16:28:10 +0300

    sss

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,39 +1,45 @@
+#!/usr/bin/python3
+
 import csv
 import requests
 import sys
 
-def getEmployeeAndTodo(id):
-    id = int(id)
-    url = f"https://jsonplaceholder.typicode.com/users/{id}/todos"
-    res = requests.get(url)
-    todos = res.json()
 
-    url = f"https://jsonplaceholder.typicode.com/users/{id}"
-    res = requests.get(url)
-    employee = res.json()
+employee_id = int(sys.argv[1])
 
-    completed_tasks = sum(1 for todo in todos if todo['completed'])
-    all_tasks = 0
 
-    for i in todos:
-        if i['userId'] == id:
-            total_tasks += 1
+def main():
+    # Define the API endpoints
+    base_url = "https://jsonplaceholder.typicode.com"
+    employee_url = f"{base_url}/users/{employee_id}"
+    todo_url = f"{base_url}/users/{employee_id}/todos"
 
-    print(f"Employee {employee['name']} is done with tasks({completed_tasks}/{all_tasks}):")
-    for todo in todos:
-        if todo['completed']:
-            print(f"\t {todo['title']}")
+    try:
+        employee_response = requests.get(employee_url)
+        employee_data = employee_response.json()
+        employee_name = employee_data['name']
 
-    with open(f'{id}.csv', 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        writer.writerow(['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE'])
-        for todo in todos:
-            writer.writerow([id, employee['name'], todo['completed'], todo['title']])
+        todo_response = requests.get(todo_url)
+        todo_data = todo_response.json()
 
-if __name__ == '__main__':
+        csv_filename = f"{employee_id}.csv"
+        with open(csv_filename, 'w', newline='') as csvfile:
+            csv_writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+
+            for task in todo_data:
+                task_completed_status = "True" if task['completed'] else "False"
+                task_title = task['title']
+                csv_writer.writerow([employee_id, employee_name, task_completed_status, task_title])
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+    except KeyError:
+        print(f"Error: User with ID {employee_id} not found.")
+
+
+if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print(f"what is the id: {sys.argv[0]} EMPLOYEE_ID")
+        print(" Usage:  CSV.py USER_ID")
         sys.exit(1)
 
-    id = sys.argv[1]
-    getEmployeeAndTodo(id)
+    main()
